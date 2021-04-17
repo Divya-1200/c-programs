a934a3d7cbb4399c28eba7c3645df2f087090db8
Divya-1200 <divya07msd@example.com>
2021-04-17 12:13:20 +0530

    Updates for 2021-04-17

--- printf-demo.c
+++ printf-demo.c
@@ -1,9 +1,18 @@
 //gcc -o my_program printf-demo.c
 	// ./my_program
-
+/*
 #include<stdio.h>
 int main()
 {
 	printf("%d",printf("2braces.com"));
+	return 0;
+} 
+*/
+
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	printf("%d",a);
 	return 0;
 }
